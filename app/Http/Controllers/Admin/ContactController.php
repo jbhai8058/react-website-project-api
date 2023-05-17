ffdeb8495038c571084645d1a3edb393b24a7f18
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-18 00:01:12 +0500

    correct the error

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -10,17 +10,24 @@ class ContactController extends Controller
 {
     public function onContactSend(Request $request){
 
+        $ContactArray = json_decode($request->getContent(),true);
+
+        $name = $ContactArray['name'] ?? null;
+        $email = $ContactArray['email'] ?? null;
+        $message = $ContactArray['message'] ?? null;
+
         $result = Contact::insert([
-            'name' => $request->name,
-            'email' => $request->email,
-            'message' => $request->message,
+            'name' => $name,
+            'email' => $email,
+            'message' => $message,
         ]);
 
-        if ($result == true) {
+        if ($result === true) {
             return 1;
         } else {
             return 0;
         }
 
     } // end method
+
 }
