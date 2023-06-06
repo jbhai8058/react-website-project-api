76922223b66682751188d5f17a163e14a6b18ab9
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-06-07 00:44:10 +0500

    setup backend  dashboard

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,6 @@
+ <div class="footer">
+            <div class="copyright">
+                <!-- <p>Copyright © Designed &amp; Developed by <a href="https://www.easylearningbd.com//" target="_blank">Easy Learning</a> 2021</p> -->
+                <p>Copyright © Designed &amp; Developed by <a href="" target="_blank">JRS</a> 2021</p>
+            </div>
+        </div>
